856ae03f0fe782c766a2044e9234172787fcef15
hadiebrahimnia <hadiebrahimnia@gmail.com>
2025-12-15 21:57:33 +0330

    debug

--- clinic/settings.py
+++ clinic/settings.py
@@ -18,7 +18,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # تنظیمات امنیتی و هاست (برای پروداکشن)
 # =============================================================================
 SECRET_KEY = env('SECRET_KEY')
-SECRET_DEBUG = env('DEBUG')
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 # =============================================================================
